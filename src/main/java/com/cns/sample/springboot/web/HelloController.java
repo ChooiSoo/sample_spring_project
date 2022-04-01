967a3e7e0c0309a0bc09b597d2d8ca033be5aaf9
chooisoo <namsu722@naver.com>
2022-04-01 18:02:12 +0900

    main/controller 생성

---
+++ src/main/java/com/cns/sample/springboot/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.cns.sample.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
